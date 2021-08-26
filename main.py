72e8a17514346beaeb64230285799d2961b06c45
mscheuer92 <michelle.scheuer@wexinc.com>
2021-08-26 19:49:26 -0400

    Update files to count all examples and produce total number

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from api_tests import ApiCount
 
 
 def output():
+    print("*** MICROSERVICE TESTS *** \n")
     a = ApiCount()
     a.address_occurrences()
     a.auth_occurrences()
     a.exchange_occurrences()
     a.ggg_occurrences()
     a.notification_occurrences()
-    a.transporterator_occurrences()
+    a.transport_occurrences()
 
+    print("*** UI TESTS *** \n")
     b = UiTests()
     b.dsp_occurrences()
     b.apigee_occurrences()
     b.service_occurrences()
-
 
 output()
